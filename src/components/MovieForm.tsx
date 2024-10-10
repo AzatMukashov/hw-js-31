295ae0f6d74d5d25aed5c931850773d02bb2060e
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-10-10 07:45:28 +0600

    начал практически заново, так как была ошибка с конкретизацией записи которую хотел удалить или отредактировать. Убрал импорт файла типизации так как он уже не нужен, убрал дефолтные значения для айди и имени, типизировал событие, переименовал переменную и функцию, добавил проверку на пустоту с форматированием пробелов, убрал placeholder его не видно, а стилизовать лень.

--- src/components/MovieForm.tsx
+++ src/components/MovieForm.tsx
@@ -1,24 +1,25 @@
 import React, { useState } from 'react';
-import { Movie } from '../types';
 
 interface MovieFormProps {
-  addMovie: (movie: Movie) => void;
+  addMovie: (name: string) => void;
 }
 
 const MovieForm: React.FC<MovieFormProps> = ({addMovie}) => {
-  const [movie, setMovie] = useState<Movie>({id: 0, title: ''});
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const {name, value} = e.target;
-    setMovie({...movie, [name]: value});
-  };
+  const [newMovie, setNewMovie] = useState('');
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    addMovie(movie);
-    setMovie({id: 0, title: ''});
+    if (newMovie.trim()) {
+      addMovie(newMovie);
+      setNewMovie('');
+    }
   }
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" name="title" value={movie.title} onChange={handleChange} placeholder="title" required/>
+      <input
+        type="text"
+        value={newMovie}
+        onChange={(e) => setNewMovie(e.target.value)}
+        required/>
       <button type='submit'>Add</button>
     </form>
   );
