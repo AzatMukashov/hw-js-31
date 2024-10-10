32a76b281f7c4151467beee6b52ac59d994b12e6
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-10-10 13:45:06 +0600

    Подключил axios и компоненты для вывода шуток. Создал состояние для них, функцию с асинхронным GET запросом к API для получения случайной шутки, где есть обновление состояния и отладка ошибок. Хук который выполняет запрос при первом рендере компонента, ну и разметка с выводом шуток

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MovieList from './components/MovieList.tsx';
+import axios from 'axios';
+import Joke from './components/haha/Joke.tsx';
+import ButtonJoke from './components/haha/ButtonJoke.tsx';
 
 const App: React.FC = () => {
+  const [joke, setJoke] = useState('');
+  const fetchJoke = async () => {
+    try {
+      const response = await axios.get('https://api.chucknorris.io/jokes/random');
+      setJoke(response.data.value);
+    } catch (error) {
+      console.error('error form get joke:', error);
+    }
+  };
+  useEffect(() => {
+    fetchJoke();
+  }, []);
   return (
     <>
       <MovieList/>
+      <h1>Joke this day</h1>
+      <Joke joke={joke}/>
+      <ButtonJoke fetchJoke={fetchJoke}/>
     </>
   );
 };
