446bfd0b0c8ec81e30c475df91e6f3764f87f14b
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-10-10 07:59:11 +0600

    начал практически заново, так как была ошибка с конкретизацией записи которую хотел удалить или отредактировать. Импортировал компонент с формой, импортировал файл типизации, убрал интерфейс. Типизировал состояние, добавил функцию добавления, функцию обновления и функцию удаления. Разметка с выводом объекта через цикл

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,23 +1,39 @@
-import React from 'react';
-import { Movie } from '../types';
+import React, { useState, useCallback } from 'react';
 import MovieItem from './MovieItem.tsx';
-interface MovieListProps {
-  movies: Movie[];
-  updateMovie: (movie: Movie) => void;
-  deleteMovie: (id: number) => void;
-}
-const MovieList: React.FC<MovieListProps> = ({movies, updateMovie, deleteMovie}) => {
+import MovieForm from './MovieForm.tsx';
+import { Movie } from '../types';
+
+const MovieList: React.FC = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const addMovie = (name: string) => {
+    setMovies([...movies, {id: Date.now(), name}]);
+  };
+
+  const updateMovie = useCallback((id: number, name: string) => {
+    setMovies((prevMovies) =>
+      prevMovies.map((movie) => (movie.id === id ? {...movie, name} : movie))
+    );
+  }, []);
+
+  const deleteMovie = useCallback((id: number) => {
+    setMovies((prevMovies) => prevMovies.filter((movie) => movie.id !== id));
+  }, []);
+
   return (
-    <ul>
-      {movies.map((movie) => (
-        <MovieItem
-          key={movie.id}
-          movie={movie}
-          updateMovie={updateMovie}
-          deleteMovie={deleteMovie}
-        />
-      ))}
-    </ul>
+    <>
+      <MovieForm addMovie={addMovie}/>
+      <ul>
+        {movies.map((movie) => (
+          <MovieItem
+            key={movie.id}
+            movie={movie}
+            updateMovie={updateMovie}
+            deleteMovie={deleteMovie}
+          />
+        ))}
+      </ul>
+    </>
   );
 };
 
