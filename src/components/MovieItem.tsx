e0005a5bf9c7b38e3e08853ebec06f488cfc5fca
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-10-10 08:23:42 +0600

    в тз было написано список я на этом зациклился, потом при стилизации снова посмотрел на пример и удалил li. Добавил иконку как в примере

--- src/components/MovieItem.tsx
+++ src/components/MovieItem.tsx
@@ -1,16 +1,19 @@
 import React, { memo } from 'react';
-import {MovieItemProps} from '../types';
+import { MovieItemProps } from '../types';
+import close from '../assets/close.png';
 
-const MovieItem: React.FC<MovieItemProps> = memo(({ movie, updateMovie, deleteMovie }) => {
+const MovieItem: React.FC<MovieItemProps> = memo(({movie, updateMovie, deleteMovie}) => {
   return (
-    <li>
+    <div>
       <input
         type="text"
         value={movie.name}
         onChange={(e) => updateMovie(movie.id, e.target.value)}
       />
-      <button onClick={() => deleteMovie(movie.id)}>Delete</button>
-    </li>
+      <button onClick={() => deleteMovie(movie.id)}>
+        <img src={close} alt="close" width='31px'/>
+      </button>
+    </div>
   );
 });
 
