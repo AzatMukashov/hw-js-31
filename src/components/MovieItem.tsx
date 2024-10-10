75b79ddd6209cd4204cf77cb91908d641cbb162f
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-10-10 06:33:29 +0600

    начал практически заново, так как была ошибка с конкретизацией записи которую хотел удалить или отредактировать. Убрал все use^..., добавил новый интерфейс из файла типизации, убрал старый интерфейс, правильно использовал мемо, разметка с использованием пропсов и функцией изменения

--- src/components/MovieItem.tsx
+++ src/components/MovieItem.tsx
@@ -1,29 +1,17 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { Movie } from '../types';
+import React, { memo } from 'react';
+import {MovieItemProps} from '../types';
 
-interface MovieProps {
-  movie: Movie;
-  updateMovie: (movie: Movie) => void;
-  deleteMovie: (id: number) => void;
-}
-
-const MovieItem: React.FC<MovieProps> = ({movie, updateMovie, deleteMovie}) => {
-  const [editMovie, setEditMovie] = useState(movie);
-  useEffect(() => {
-    updateMovie(editMovie);
-  }, [editMovie, updateMovie]);
-  const handleEditMovie = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    const {name, value} = e.target;
-    setEditMovie({...editMovie, [name]: value});
-  }, [editMovie]);
+const MovieItem: React.FC<MovieItemProps> = memo(({ movie, updateMovie, deleteMovie }) => {
   return (
     <li>
-      <input type="text" name='title' value={editMovie.title} onChange={handleEditMovie}/>
+      <input
+        type="text"
+        value={movie.name}
+        onChange={(e) => updateMovie(movie.id, e.target.value)}
+      />
       <button onClick={() => deleteMovie(movie.id)}>Delete</button>
     </li>
   );
-};
-
-export default React.memo(MovieItem, (prevProps, nextProps) => {
-  return prevProps.movie === nextProps.movie;
 });
+
+export default MovieItem;
